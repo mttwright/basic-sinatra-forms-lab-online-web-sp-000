9e9680ec6a3776bdff1822dcdddb495d93125270
Matt Wright <mttwright@gmail.com>
2021-05-25 12:21:48 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,6 +1,15 @@
 require 'sinatra/base'
 
 class App < Sinatra::Base
+  
+  get '/newteam' do
+    erb :newteam
+  end
+  
+  post '/toast' do
+    erb :team
+    #"#{@params_arram}"
+  end
 
 
 end
